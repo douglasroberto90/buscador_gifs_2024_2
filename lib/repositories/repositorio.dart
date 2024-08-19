0c34ea090b155783b97fe8cd466c472a6d1eba0e
douglasroberto90 <douglas_roberto90@hotmail.com>
2024-08-19 20:38:04 -0300

    updates

--- lib/repositories/repositorio.dart
+++ lib/repositories/repositorio.dart
@@ -3,29 +3,28 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class Repositorio {
+  static late String apiKey;
 
-  late String apiKey;
-  
-  Future<void> inicializarDotEnv() async {
+  static Future<void> _inicializarDotEnv() async {
     await dotenv.load(fileName: "keys.env");
     apiKey = dotenv.get("API_KEY");
   }
-  
-  Future<Map<String, dynamic>> buscarTrending() async {
-    inicializarDotEnv();
+
+  static Future<Map<String, dynamic>> buscarTrending() async {
+    _inicializarDotEnv();
     final response = await http.get(Uri.parse(
         "https://api.giphy.com/v1/gifs/trending?api_key=$apiKey&limit=20&offset=0&rating=g&bundle=messaging_non_clips"));
     return jsonDecode(response.body);
   }
-  
-  Future<Map<String, dynamic>> buscarGifs(String termoBusca) async {
-    inicializarDotEnv();
-    Uri url = Uri.https("api.giphy.com","/v1/gifs/search",{
-      "api_key":apiKey,
+
+  static Future<Map<String, dynamic>> buscarGifs(String termoBusca) async {
+    _inicializarDotEnv();
+    Uri url = Uri.https("api.giphy.com", "/v1/gifs/search", {
+      "api_key": apiKey,
       "q": termoBusca,
-      "rating":"g",
-      "lang":"pt",
-      "bundle":"messaging_non_clips"
+      "rating": "g",
+      "lang": "pt",
+      "bundle": "messaging_non_clips"
     });
     final response = await http.get(url);
     return jsonDecode(response.body);
